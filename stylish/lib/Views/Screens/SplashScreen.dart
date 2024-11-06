0325ceb4c1433f3ca09eeb88c4bb5c275a29e860
Anet Taj <58222128+anettaj@users.noreply.github.com>
2024-11-07 01:31:14 +0530

    Splash screen added

--- stylish/lib/Views/Screens/SplashScreen.dart
+++ stylish/lib/Views/Screens/SplashScreen.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    // _navigateToNextScreen();
+  }
+
+  // void _navigateToNextScreen() async {
+  //   await Future.delayed(Duration(seconds: 3)); // 3-second delay
+  //
+  //   // Check if user has completed onboarding
+  //   bool isOnboardingCompleted = false; // Replace with actual onboarding check logic
+  //   if (isOnboardingCompleted) {
+  //     Get.off(() => HomeScreen());
+  //   } else {
+  //     Get.off(() => OnBoardingScreen());
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
